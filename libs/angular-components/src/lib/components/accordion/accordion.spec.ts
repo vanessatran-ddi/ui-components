030e33782463792605bbeed0d94505113b7fe1c3
Vanessa Tran <Vanessa.M.Tran@gov.ab.ca>
2024-05-08 13:59:40 -0600

    commit to checkout

--- libs/angular-components/src/lib/components/accordion/accordion.spec.ts
+++ libs/angular-components/src/lib/components/accordion/accordion.spec.ts
@@ -1,17 +1,45 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { GoAAccordion } from "./accordion";
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 let component: GoAAccordion;
 let fixture: ComponentFixture<GoAAccordion>;
 
-beforeEach(() => {
-  TestBed.configureTestingModule({
-    imports: [GoAAccordion],
+@Component({
+  template: `
+    <goa-accordion [heading]="heading" [headingsize]="headingSize">
+      test content
+    </goa-accordion>`
+})
+class TestAccordionComponent {
+  heading: string = "Heading";
+  headingSize: string = "medium";
+}
+
+describe("Accordion", () => {
+  let fixture: ComponentFixture<TestAccordionComponent>;
+  let accordion;
+
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, GoAAccordion],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [TestAccordionComponent]
+    });
   });
-  fixture = TestBed.createComponent(GoAAccordion);
-  component = fixture.componentInstance;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestAccordionComponent);
+    accordion = fixture.debugElement.children[0].componentInstance;
+    fixture.detectChanges();
+    console.log(fixture.nativeElement.innerHTML);
+  });
+  it("should render", () => {
+    fixture.detectChanges();
+    // debug component html
+    console.log(fixture.nativeElement.innerHTML);
+  });
 });
 
-it("should render", () => {
-  expect(component).toBeTruthy();
-});
+
+
